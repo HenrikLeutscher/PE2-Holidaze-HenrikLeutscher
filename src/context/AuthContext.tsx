a57d478c2a0016b567924b9df4338dfb5cd6b0e6
HenrikLeutscher <HenrikHezo@hotmail.com>
2026-05-22 16:31:22 +0200

    API Call for Profile Page

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -11,19 +11,50 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const storedUser = localStorage.getItem("user");
     return storedUser ? JSON.parse(storedUser) : null;
   });
-  console.log(user);
 
   const [token, setToken] = useState<string | null>(() => {
     return localStorage.getItem("token");
   });
 
+  const fetchFullProfile = async (username: string, token: string) => {
+    try {
+      const response = await fetch(
+        `https://v2.api.noroff.dev/holidaze/profiles/${username}?_venues=true&_bookings=true`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+            "X-Noroff-API-Key": import.meta.env.VITE_API_KEY,
+          },
+        },
+      );
+      if (!response.ok) {
+        throw new Error(`Failed to fetch profile: ${response.status}`);
+      }
+      const fullProfileData = await response.json();
+      return fullProfileData.data;
+    } catch (error) {
+      console.error("Error fetching full profile:", error);
+      return null;
+    }
+  };
+
   const login = async (input: Login) => {
     try {
-      const data = await loginReq(input);
-      setUser(data);
-      setToken(data.accessToken);
-      localStorage.setItem("user", JSON.stringify(data));
-      localStorage.setItem("token", data.accessToken);
+      const loginData = await loginReq(input);
+
+      const fullProfile = await fetchFullProfile(
+        loginData.name,
+        loginData.accessToken,
+      );
+      if (!fullProfile) {
+        throw new Error("Failed to fetch full profile after login.");
+      }
+
+      setUser(fullProfile);
+      setToken(loginData.accessToken);
+      localStorage.setItem("user", JSON.stringify(fullProfile));
+      localStorage.setItem("token", loginData.accessToken);
     } catch (err: unknown) {
       if (err instanceof Error) {
         throw new Error("Invalid Email or Password");
